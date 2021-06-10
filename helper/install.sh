d24eb7c0878feb9ad711919ddec25691f0305d52
Tianyu Liu <info@jamesvillage.dev>
2021-06-10 21:07:06 +0200

    Add db preparation in install

--- helper/install.sh
+++ helper/install.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+# Prepare database
+sudo mysql -e "CREATE USER IF NOT EXISTS 'piggysaving'@'localhost' IDENTIFIED BY 'piggysaving';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'piggysaving'@'localhost';"
+sudo mysql -e "FLUSH PRIVILEGES"
+
