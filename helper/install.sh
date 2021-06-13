ebec7743676c318c5dacebd2267810a81812e805
Tianyu Liu <info@jamesvillage.dev>
2021-06-13 22:14:02 +0200

    Add installation script till frontend

--- helper/install.sh
+++ helper/install.sh
@@ -13,32 +13,43 @@
 # sudo mysql -p piggysaving -e "CREATE TABLE piggysaving(savingDate DATE, amount FLOAT, saved BOOLEAN, PRIMARY KEY (savingDate))"
 
 
-echo 'Enter mail address for mailing service, leave empty if you want to disable mailing service.'
-echo 'Enter the mail from address for mailing service:'
-read mailFrom
-echo 'Enter the mail to address for mailing service:'
-read mailTo
+# echo 'Enter mail address for mailing service, leave empty if you want to disable mailing service.'
+# echo 'Enter the mail from address for mailing service:'
+# read mailFrom
+# echo 'Enter the mail to address for mailing service:'
+# read mailTo
 
-if [ -f "/etc/systemd/system/piggysaving.service" ]; then
-    echo 'Disable existing service'
-    sudo systemctl stop piggysaving.service
-    sudo systemctl disable piggysaving.service
-    sudo rm /etc/systemd/system/piggysaving.service
-fi
+# if [ -f "/etc/systemd/system/piggysaving.service" ]; then
+#     echo 'Disable existing service'
+#     sudo systemctl stop piggysaving.service
+#     sudo systemctl disable piggysaving.service
+#     sudo rm /etc/systemd/system/piggysaving.service
+# fi
 
-mkdir -p $HOME/.local/piggysaving
+# mkdir -p $HOME/.local/piggysaving
 
-cp -r ../backend/{restserver.py,saving.py} $HOME/.local/piggysaving
+# cp -r ../backend/{restserver.py,saving.py} $HOME/.local/piggysaving
 
-sed -i 's/MAIL_FROM = ""/MAIL_FROM = "'"$mailFrom"'"/g' $HOME/.local/piggysaving/saving.py
-sed -i 's/MAIL_TO = ""/MAIL_TO = "'"$mailTo"'"/g' $HOME/.local/piggysaving/saving.py
+# sed -i 's/MAIL_FROM = ""/MAIL_FROM = "'"$mailFrom"'"/g' $HOME/.local/piggysaving/saving.py
+# sed -i 's/MAIL_TO = ""/MAIL_TO = "'"$mailTo"'"/g' $HOME/.local/piggysaving/saving.py
 
-echo 'Copy service file'
-sudo cp piggysaving.service /etc/systemd/system
+# echo 'Copy service file'
+# sudo cp piggysaving.service /etc/systemd/system
 
-echo 'Alter service file'
-sudo sed -i 's/User=/User='"$USER"'/g' /etc/systemd/system/piggysaving.service
-sudo sed -i 's+WorkingDirectory=+WorkingDirectory='"$HOME"'/.local/piggysaving+g' /etc/systemd/system/piggysaving.service
+# echo 'Alter service file'
+# sudo sed -i 's/User=/User='"$USER"'/g' /etc/systemd/system/piggysaving.service
+# sudo sed -i 's+WorkingDirectory=+WorkingDirectory='"$HOME"'/.local/piggysaving+g' /etc/systemd/system/piggysaving.service
 
-sudo systemctl enable piggysaving.service
-sudo systemctl start piggysaving.service
+# sudo systemctl enable piggysaving.service
+# sudo systemctl start piggysaving.service
+
+# Install frontend
+echo 'Install frontend to /var/www/piggysaving'
+sudo rm -rf /var/www/piggysaving
+pushd ../frontend/piggysaving
+npm install
+npm run build
+sudo mkdir -p /var/www/piggysaving
+sudo cp -r dist/* /var/www/piggysaving
+sudo chown -R www-data:www-data /var/www/piggysaving
+popd
