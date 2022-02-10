dd97fe8e53441513754e9d62ddac5f32717920a8
Tianyu Liu <liu.tianyu93@hotmail.com>
2022-02-10 15:29:10 +0000

    Update requirements with fixed version
    Install now make python backen using venv
    
    Disable mailing for now

--- helper/install.sh
+++ helper/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+BASH_DIR=`dirname $BASH_SOURCE`
+BACKEND_TARGET="$HOME/.local/piggysaving"
 # Install packages
 sudo apt update
-sudo apt install mailutils mariadb-server python3
+sudo apt install mailutils mariadb-server python3 python3-venv
 
 # Prepare database
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS piggysaving"
@@ -18,11 +20,11 @@ sudo mysql piggysaving -e "CREATE TABLE piggysaving(savingDate DATE,
                             type varchar(255),
                             PRIMARY KEY (savingDate, sequence)) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
-echo 'Enter mail address for mailing service, leave empty if you want to disable mailing service.'
-echo 'Enter the mail from address for mailing service:'
-read mailFrom
-echo 'Enter the mail to address for mailing service:'
-read mailTo
+# echo 'Enter mail address for mailing service, leave empty if you want to disable mailing service.'
+# echo 'Enter the mail from address for mailing service:'
+# read mailFrom
+# echo 'Enter the mail to address for mailing service:'
+# read mailTo
 
 if [ -f "/etc/systemd/system/piggysaving.service" ]; then
     echo 'Disable existing service'
@@ -31,32 +33,41 @@ if [ -f "/etc/systemd/system/piggysaving.service" ]; then
     sudo rm /etc/systemd/system/piggysaving.service
 fi
 
-mkdir -p $HOME/.local/piggysaving
+mkdir -p $BACKEND_TARGET
 
-cp -r ../backend/{restserver.py,saving.py} $HOME/.local/piggysaving
+python3 -m venv $BACKEND_TARGET/venv
 
-sed -i 's/MAIL_FROM = ""/MAIL_FROM = "'"$mailFrom"'"/g' $HOME/.local/piggysaving/saving.py
-sed -i 's/MAIL_TO = ""/MAIL_TO = "'"$mailTo"'"/g' $HOME/.local/piggysaving/saving.py
+source $BACKEND_TARGET/venv/bin/activate
+
+pip install -r $BASH_DIR/requirements.txt
+
+deactivate
+
+cp -r $BASH_DIR/../backend/{restserver.py,saving.py} $BACKEND_TARGET
+
+# sed -i 's/MAIL_FROM = ""/MAIL_FROM = "'"$mailFrom"'"/g' $BACKEND_TARGET/saving.py
+# sed -i 's/MAIL_TO = ""/MAIL_TO = "'"$mailTo"'"/g' $BACKEND_TARGET/saving.py
 
 echo 'Copy service file'
-sudo cp piggysaving.service /etc/systemd/system
+sudo cp $BASH_DIR/piggysaving.service /etc/systemd/system
 
 echo 'Alter service file'
 sudo sed -i 's/User=/User='"$USER"'/g' /etc/systemd/system/piggysaving.service
 sudo sed -i 's+WorkingDirectory=+WorkingDirectory='"$HOME"'/.local/piggysaving+g' /etc/systemd/system/piggysaving.service
+sudo sed -i 's+ExecStart=~+ExecStart='"$HOME"'+g' /etc/systemd/system/piggysaving.service
 
 sudo systemctl enable piggysaving.service
 sudo systemctl start piggysaving.service
 
-# Install frontend
-echo 'Install frontend to /var/www/piggysaving'
-sudo rm -rf /var/www/piggysaving
-pushd ../frontend/piggysaving
-npm install
-npm run build
-sudo mkdir -p /var/www/piggysaving
-sudo cp -r dist/* /var/www/piggysaving
-sudo chown -R www-data:www-data /var/www/piggysaving
-popd
+# # Install frontend
+# echo 'Install frontend to /var/www/piggysaving'
+# sudo rm -rf /var/www/piggysaving
+# pushd ../frontend/piggysaving
+# npm install
+# npm run build
+# sudo mkdir -p /var/www/piggysaving
+# sudo cp -r dist/* /var/www/piggysaving
+# sudo chown -R www-data:www-data /var/www/piggysaving
+# popd
 
 echo 'Please modify your server (NGINX/Apache) settings.'
